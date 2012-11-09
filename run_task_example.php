9173d63053a938a26fe71ea91c50b9f5afc6869f
adorsk <adorsk>
2012-11-09 13:34:49 -0500

    added example task run script, added error handling for task start

---
+++ run_task_example.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Example script demonstrating how to run a task from the CLI.
+ * This script would be executed by drush.
+ */
+
+// Create task node.
+$node = new stdClass();
+$node->type = 'xtask';
+node_object_prepare($node);
+$node->title = "My Task";
+$node->language = LANGUAGE_NONE;LANGUAGE_NONE;LANGUAGE_NONE;LANGUAGE_NONE;
+
+$config = array(
+  "command" => "/some/command/to/run.sh"
+);
+$node->xtasks_config[LANGUAGE_NONE][0]['value'] = json_encode($config);
+node_save($node);
+
+// Run task.
+xtasks_run_task($node->nid);
